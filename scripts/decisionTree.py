3e6d55b2ac876b4ca4064f391aa19af63e3ade5c
lynnha577 <lynnha577@gmail.com>
2025-10-03 18:07:59 -0700

    made a seperate function for plotting accuracy

--- scripts/decisionTree.py
+++ scripts/decisionTree.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from get_features import get_dataframes, get_labels
 import os
+from sklearn.ensemble import RandomForestClassifier
+
 
 def make_decisionTree(X, Y, max_leaf_nodes):
     """
@@ -30,7 +32,7 @@ def make_decisionTree(X, Y, max_leaf_nodes):
     plt.show()
 
 
-def get_acc(X_train, y_train, X_test, y_test, list_nodes):
+def get_acc(X_train, y_train, X_test, y_test, list_nodes, classifierName):
     """
     loops through different numbers of max leaf nodes and fits a decision tree classifier
     and records the train accuracy and test accuracy.
@@ -41,6 +43,7 @@ def get_acc(X_train, y_train, X_test, y_test, list_nodes):
         X_test (array): a 2-dimentional array that stores the samples for test features. shape = (num_samples, num_features)
         y_test (list): a 1-dimentional list that stores the samples for test labels. shape = (num_samples)
         list_nodes (list): a list of the nodes we are testing with.
+        classifierName (String): A string indicating the type of classifier to use
 
     Returns:
         train_accs (list): a list of the train scores from each node.
@@ -50,54 +53,61 @@ def get_acc(X_train, y_train, X_test, y_test, list_nodes):
     test_accs = []
 
     for node in list_nodes:
-        clf = DecisionTreeClassifier(max_leaf_nodes=node, random_state=0)
+        if classifierName == "DecisionTree":
+            clf = DecisionTreeClassifier(max_leaf_nodes=node, random_state=0)
+        elif classifierName == "RandomForest":
+            clf = RandomForestClassifier(max_leaf_nodes=node, random_state=0)
+        else:
+            raise NotImplementedError
         clf.fit(X_train, y_train)
         train_accs.append(clf.score(X_train, y_train))
         test_accs.append(clf.score(X_test, y_test))
+        
 
     return train_accs, test_accs
 
-def decisionTreeAnalysis(X, Y, labelNames, max_nodes = 24):
+def decisionTreeAnalysis(X, Y, labelNames, classifierName, max_nodes = 24):
+    """ graphs a comparison of train and test accuracy using labels provided and plots a confusion
+    matrix of the train and test with 12 and 24 nodes.
+
+    Args:
+        X (dataframe): features we are using to predict
+        Y (dataframe): labels we are trying to predict 
+        labelNames (string): uses it to name the files
+        classifierName (String): determines which chlassifier to use
+        max_nodes (int, optional): amount of nodes to use. Defaults to 24.
+
+    Raises:
+        NotImplementedError: _description_
+    """
+    # place to put results
     results_dir = f"{os.pardir}/results/"
     X_train, X_test, y_train, y_test = train_test_split(
         X, Y, test_size=0.20, random_state=42)
+    plot_acc(X, Y, labelNames, classifierName, max_nodes = 24)
 
-    list_nodes = np.arange(2, max_nodes, 2)
-
-    train_acc, test_acc = get_acc(X_train, y_train, X_test, y_test, list_nodes)
-    print(train_acc)
-    print(test_acc)
-
-    fig, ax  = plt.subplots()
-
-    plt.plot(list_nodes, train_acc, label = "train")
-    plt.plot(list_nodes, test_acc, label = "test")
-    plt.xlabel("number of nodes")
-    plt.ylabel("accuracy")
-    plt.title("decision tree performance on ephys data")
-    plt.legend()
-    plt.show
-
-    plt.savefig(f"{results_dir}DecisionTreePerformance{labelNames}_ephys.png")
-
-    #12 nodes
-    fig, ax  = plt.subplots(nrows=2, ncols=2, sharex = True, sharey=True)
-
-    clf = DecisionTreeClassifier(max_leaf_nodes=12, random_state=0)
+    # determines which classifier to use
+    if classifierName == "DecisionTree":
+        clf = DecisionTreeClassifier(max_leaf_nodes=max_nodes, random_state=0)
+    elif classifierName == "RandomForest":
+        clf = RandomForestClassifier(max_leaf_nodes=max_nodes, random_state=0)
+    else:
+        raise NotImplementedError
+    
+    #makes a confusion matrix on 12 nodes train and test
     clf.fit(X_train, y_train)
-
     y_pred = clf.predict(X_train)
     from sklearn.metrics import confusion_matrix
     matrix = confusion_matrix(y_train, y_pred)
-    print(matrix)
 
     from sklearn.metrics import ConfusionMatrixDisplay
+    fig, ax  = plt.subplots(nrows=2, ncols=2, sharex = True, sharey=True)
     ax[0,0].set_title('12 node on train')
     ax[0,1].set_title("12 node on test")
     ConfusionMatrixDisplay.from_estimator(clf, X_train, y_train, ax = ax[0,0])
     ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test, ax= ax[0, 1])
 
-    # 24 nodes 
+    # makes confusion matrix on 24 nodes on train and test
 
     clf2 = DecisionTreeClassifier(max_leaf_nodes=24, random_state=0)
     clf2.fit(X_train, y_train)
@@ -105,7 +115,6 @@ def decisionTreeAnalysis(X, Y, labelNames, max_nodes = 24):
     y_pred = clf2.predict(X_train)
     from sklearn.metrics import confusion_matrix
     matrix = confusion_matrix(y_train, y_pred)
-    print(matrix)
 
     ax[1,0].set_title('24 node on train')
     ax[1,1].set_title("24 node on test")
@@ -115,8 +124,33 @@ def decisionTreeAnalysis(X, Y, labelNames, max_nodes = 24):
     fig.suptitle("comparing 12 node and 24 node decision trees \n on ephys data")
     plt.tight_layout()
 
+    # saves plot as a png file in results
     plt.savefig(f"{results_dir}comparingNodes_ephys{labelNames}.png")
 
+def plot_acc(X, Y, labelNames, classifierNames, max_nodes = 24):
+    # spliting data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, Y, test_size=0.20, random_state=42)
+    print(f"aa {len(X_train)}")
+
+    list_nodes = np.arange(2, max_nodes, 2)
+
+    # get accuracy for train and test
+    train_acc, test_acc = get_acc(X_train, y_train, X_test, y_test, list_nodes, classifierNames)
+    print(train_acc)
+    print(test_acc)
+
+    fig, ax  = plt.subplots()
+    # plots a graph comparing train and test on accuracy
+    plt.plot(list_nodes, train_acc, label = "train")
+    plt.plot(list_nodes, test_acc, label = "test")
+    plt.xlabel("number of nodes")
+    plt.ylabel("accuracy")
+    plt.title("decision tree performance on ephys data")
+    plt.legend()
+    plt.show
+    results_dir = f"{os.pardir}/results/"
+    plt.savefig(f"{results_dir}DecisionTreePerformance{labelNames}_ephys.png")
 
 def main():
     labels = get_labels()
