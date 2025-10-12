43805f82bc0a0500fdce59065bf5cc665674fbe3
lynnha577 <lynnha577@gmail.com>
2025-10-11 20:53:10 -0700

    plot morphology function

--- scripts/plot_data.py
+++ scripts/plot_data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from allensdk.core.cell_types_cache import CellTypesCache
+from allensdk.core.swc import Marker
+import pprint
 
 def plot_data(cell_specimen_id, sweep_number, tstart, tend):
     """
@@ -52,6 +54,39 @@ def plot_data(cell_specimen_id, sweep_number, tstart, tend):
     plt.show()
     return axes
 
+def plot_morphology(cell_id):
+    ctc = CellTypesCache(manifest_file='cell_types/manifest.json')
+    morphology = ctc.get_reconstruction(cell_id) 
+    markers = ctc.get_reconstruction_markers(cell_id) 
+
+    # the compartment list has all of the nodes in the file
+
+    fig, axes = plt.subplots(1, 2, sharey=True, sharex=True)
+    axes[0].set_aspect('equal', 'box')
+    axes[1].set_aspect('equal', 'box')
+
+    # Make a line drawing of x-y and y-z views
+    for n in morphology.compartment_list:
+        for c in morphology.children_of(n):
+            axes[0].plot([n['x'], c['x']], [n['y'], c['y']], color='black')
+            axes[1].plot([n['z'], c['z']], [n['y'], c['y']], color='black')
+
+    # cut dendrite markers
+    dm = [ m for m in markers if m['name'] == Marker.CUT_DENDRITE ]
+
+    axes[0].scatter([m['x'] for m in dm], [m['y'] for m in dm], color='#3333ff')
+    axes[1].scatter([m['z'] for m in dm], [m['y'] for m in dm], color='#3333ff')
+
+    # no reconstruction markers
+    nm = [ m for m in markers if m['name'] == Marker.NO_RECONSTRUCTION ]
+
+    axes[0].scatter([m['x'] for m in nm], [m['y'] for m in nm], color='#333333')
+    axes[1].scatter([m['z'] for m in nm], [m['y'] for m in nm], color='#333333')
+
+    axes[0].set_ylabel('y')
+    axes[0].set_xlabel('x')
+    axes[1].set_xlabel('z')
+    plt.show()
 def main():
     cell_specimen_id = 501799874
     sweep_number = 35
