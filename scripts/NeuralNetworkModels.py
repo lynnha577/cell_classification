60b95fae9d3e68beb973a6bf44bad960a1c0cb54
lynnha577 <lynnha577@gmail.com>
2026-02-06 18:56:25 -0800

    lstm and mlp model

--- scripts/NeuralNetworkModels.py
+++ scripts/NeuralNetworkModels.py
@@ -8,7 +8,7 @@ class mlp_voltage_predictor(nn.Module):
     def __init__(self, input_size, output_size):
         super().__init__()
         #self.layer1 = nn.Linear(input_size, 32)
-        self.layer1 = nn.LSTM(input_size, hidden_size = 32, num_layers=1)
+        self.layer1 = nn.Linear(input_size, 32)
         #self.activationFunction = nn.ReLU()
         self.layer2 = nn.Linear(32, 8)
         self.activationFunction2 = nn.ReLU()
@@ -16,12 +16,36 @@ class mlp_voltage_predictor(nn.Module):
 
     # sets how data moves thorugh layers
     def forward(self, x):
-        x, _=self.layer1(x) # input goes through first layer
+        x = self.layer1(x) # input goes through first layer
         #x = self.activationFunction(x) # ReLU is activated
         print(x.size())
         x = self.layer2(x) # data goes through second layer
         x = self.activationFunction2(x) # ReLU is activated 
 
         x = self.layer3(x)
+        x = self.activationFunction2(x)
+        return x # output is returned.
+    
+
+class LSTM_voltage_predictor(nn.Module):
+    # sets up layers (2) and activation function (ReLU)
+    def __init__(self, input_size, output_size):
+        super().__init__()
+        #self.layer1 = nn.Linear(input_size, 32)
+        self.layer1 = nn.LSTM(input_size, hidden_size = 32, num_layers=1)
+        #self.activationFunction = nn.ReLU()
+        self.layer2 = nn.LSTM(32, 8)
+        self.activationFunction2 = nn.ReLU()
+        self.layer3 = nn.LSTM(8, output_size)
+
+    # sets how data moves thorugh layers
+    def forward(self, x):
+        x, _=self.layer1(x) # input goes through first layer
+        x = self.activationFunction2(x) # ReLU is activated
+        print(x.size())
+        x, _= self.layer2(x) # data goes through second layer
+        x = self.activationFunction2(x) # ReLU is activated 
+
+        x, _ = self.layer3(x)
         x = self.activationFunction2(x)
         return x # output is returned.
